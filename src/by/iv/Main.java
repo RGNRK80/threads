6bef0270feb54c4c7913be12db15b2cf888191cd
RGNRK80 <armhound@gmail.com>
2021-11-25 16:40:04 +0300

    2

--- src/by/iv/Main.java
+++ src/by/iv/Main.java
@@ -5,29 +5,38 @@ public class Main {
         Thread mThread = Thread.currentThread();
         System.out.println(mThread.getName());
 
-        Runnable child1= () -> {
-            for (int i = 0; i <100 ; i++) {
-                System.out.println("i'm a child");
-                }
-            };
-        for (int i = 0; i <5 ; i++) {
-            child1.run();
-        }
+//        Runnable child1= () -> {
+//            for (int i = 0; i <100 ; i++) {
+//                System.out.println("i'm a child");
+//                }
+//            };
+//        for (int i = 0; i <5 ; i++) {
+//            child1.run();
+//        }
+//
+//
+//
+//
+//        System.out.println("i'am parent");
+//        System.out.println("-------next one--------");
+//
+//        for (int i = 0; i <5 ; i++) {
+//            Egg egg1=new Egg(i);
+//            egg1.start();
+//        }
+//        Hen hen1=new Hen(1);
+//        hen1.start();
+//        hen1.join();
 
 
-
-
-        System.out.println("i'am parent");
-        System.out.println("-------next one--------");
-
-        for (int i = 0; i <5 ; i++) {
-            Egg egg1=new Egg(i);
-            egg1.start();
-        }
-        Hen hen1=new Hen(1);
-        hen1.start();
-        hen1.join();
-
+        System.out.println("Main thread started");
+        CustomerThread customerThread = new CustomerThread();
+        new Thread(customerThread, "CustomerThread").start();
+        Thread.sleep(2000);
+        System.out.println(Thread.currentThread().getName());
+        customerThread.disable();
+        Thread.sleep(1000);
+        System.out.println("Main thread finished");
 
 
 
@@ -60,5 +69,33 @@ class Egg extends Thread{
     public void run() {
         for (int i = 0; i <100 ; i++) {
             System.out.println("Egg thread: " + id + " is running-" + i);}
+    }
+}
+
+class CustomerThread implements Runnable {
+    private boolean isActive;
+
+    void disable() {
+        isActive = false;
+    }
+
+    CustomerThread() {
+        isActive = true;
+    }
+
+    public void run() {
+        String threadName = Thread.currentThread().getName();
+        System.out.println(threadName + " started");
+        int iteration = 1;
+        while (isActive) {
+            iteration++;
+            System.out.println("Customer thread is working " + iteration);
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(threadName + " finished");
     }
 }
